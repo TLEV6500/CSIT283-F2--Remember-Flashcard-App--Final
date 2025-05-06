9e0472f85b74787ee1ba13e7f8406f67c8030b17
Tim Leobert Villacin <timvillacin@gmail.com>
2025-05-07 07:49:08 +0800

    + Added core UI

---
+++ app/src/main/java/com/csit284/remember_flashcard_app__final/core/LandingActivity.kt
@@ -0,0 +1,24 @@
+package com.csit284.remember_flashcard_app__final.core
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.csit284.remember_flashcard_app__final.databinding.ActivityLandingBinding
+import com.csit284.remember_flashcard_app__final.about.AboutActivity
+import com.csit284.remember_flashcard_app__final.auth.AuthActivity
+
+class LandingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLandingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startActivity(Intent(this, AuthActivity::class.java))
+        }
+        binding.btnAbout.setOnClickListener {
+            startActivity(Intent(this, AboutActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/csit284/remember_flashcard_app__final/core/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.csit284.remember_flashcard_app__final.core
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.csit284.remember_flashcard_app__final.R
+import com.csit284.remember_flashcard_app__final.auth.AuthActivity
+import com.csit284.remember_flashcard_app__final.dashboard.DashboardActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Install splash screen before super.onCreate()
+        val splashScreen = installSplashScreen()
+        val auth = FirebaseAuth.getInstance();
+        Log.d("Firebase", "Auth initialized: ${auth.app.name}")
+
+        // Optional: Keep splash screen visible while loading data
+        splashScreen.setKeepOnScreenCondition {
+            auth.currentUser == null  // Example: Wait for auth check
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Redirect to Login or Dashboard based on auth state
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, AuthActivity::class.java))
+        } else {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+        finish() // Close MainActivity immediately
+    }
+}
